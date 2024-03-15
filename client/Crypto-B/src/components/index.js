3dbe04c278aa538ce2e074f6fc6f2c2d41a2e667
BHAVIK-RADADIYA1 <bhavikradadiyamaruti@gmail.com>
2024-03-15 09:10:46 +0530

    Basic Structure of Project

---
+++ client/Crypto-B/src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
+export { default as Coins } from "./Coins";
+export { default as CoinDetails } from "./CoinDetails";
+export { default as Hero } from "./Hero";
+export { default as Loader } from "./Loader";
+export { default as Services } from "./Services";
+export { default as Welcome } from "./Welcome";
